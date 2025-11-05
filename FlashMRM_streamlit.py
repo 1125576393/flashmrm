a288960d3e8538c6f7327ee5a4015a14978e3f15
1125576393-png <1125576393@qq.com>
2025-11-05 14:40:18 +0800

    Update FlashMRM_streamlit.py

--- FlashMRM_streamlit.py
+++ FlashMRM_streamlit.py
@@ -381,7 +381,7 @@ def run_flashmrm_calculation():
 # 主标题和Help按钮
 col_title, col_help,col_helpo = st.columns([2, 1,3])
 with col_title:
-   st.image("786a50646609813e89cc2017082525a3.png", width=250)
+   st.image("786a50646609813e89cc2017082525a3.png", width=200)
 with col_help:
     if st.button("Help", width='stretch', key="help_btn"):  
         st.session_state.show_help = not st.session_state.get('show_help', False)
@@ -631,6 +631,7 @@ if st.session_state.calculation_complete:
 st.sidebar.markdown("---")
 st.sidebar.markdown("**FlashMRM** - 质谱MRM参数优化工具")
 st.sidebar.markdown(f"当前时间: {time.strftime('%Y-%m-%d %H:%M:%S')}")
+
 
 
 
