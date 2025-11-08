04e053228d14b609f728fc0764f2897ffc15145a
1125576393-png <1125576393@qq.com>
2025-11-08 18:28:03 +0800

    Update FlashMRM_streamlit.py

--- FlashMRM_streamlit.py
+++ FlashMRM_streamlit.py
@@ -521,7 +521,7 @@ if selected_mode != st.session_state.input_mode:
 # 上传按钮
 upload_clicked = st.button(
     "Upload",
-    width=100,
+    width=300,
     key="upload_button",
     disabled=st.session_state.calculation_in_progress
 )
@@ -681,6 +681,7 @@ if st.session_state.calculation_complete:
 st.sidebar.markdown("---")
 st.sidebar.markdown("**FlashMRM** - 质谱MRM参数优化工具")
 st.sidebar.markdown(f"当前时间: {time.strftime('%Y-%m-%d %H:%M:%S')}")
+
 
 
 
